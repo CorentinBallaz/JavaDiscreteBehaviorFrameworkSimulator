f1186393274a41594a132d14b5d0fd89517e367f
MaxencepRSZ <maxenceperso@gmail.com>
2020-05-06 16:30:30 +0200

    hasnext merged done

--- test/timerTest/MergedTimerTest.java
+++ test/timerTest/MergedTimerTest.java
@@ -6,19 +6,65 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import timer.DateTimer;
+import timer.MergedTimer;
+import timer.OneShotTimer;
+import timer.PeriodicTimer;
+
 class MergedTimerTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void hasNextTestTrue() {
+		PeriodicTimer pt1 = new PeriodicTimer(0);
+		PeriodicTimer pt2 = new PeriodicTimer(0);
+		
+		MergedTimer mt = new MergedTimer(pt1, pt2);
+		
+		boolean actual = mt.hasNext();
+		
+		assertTrue(actual);
+	}
+	
+	@Test
+	void hasNextTestFalse1() {
+		PeriodicTimer pt1 = new PeriodicTimer(0);
+		OneShotTimer ost1 = new OneShotTimer(0);
+		ost1.next();
+		
+		MergedTimer mt = new MergedTimer(pt1, ost1);
+		
+		boolean actual = mt.hasNext();
+		
+		assertFalse(actual);
+	}
+	
+	@Test
+	void hasNextTestFalse2() {
+		OneShotTimer ost1 = new OneShotTimer(0);
+		OneShotTimer ost2 = new OneShotTimer(0);
+		ost1.next();
+		ost2.next();
+		
+		MergedTimer mt = new MergedTimer(ost1, ost2);
+		
+		boolean actual = mt.hasNext();
+		
+		assertFalse(actual);
+	}
+	
+	@Test
+	void nextTestFalse() {
+		OneShotTimer ost1 = new OneShotTimer(0);
+		OneShotTimer ost2 = new OneShotTimer(0);
+		ost1.next();
+		ost2.next();
+		
+		MergedTimer mt = new MergedTimer(ost1, ost2);
+		
+		boolean actual = mt.hasNext();
+		
+		assertFalse(actual);
 	}
 
 }
