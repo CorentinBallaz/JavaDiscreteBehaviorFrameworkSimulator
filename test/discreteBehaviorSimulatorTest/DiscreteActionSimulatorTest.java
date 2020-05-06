fb1cd739bce5f0de024adeccbd982bfb8ac9e11a
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2020-05-06 15:21:48 +0200

    test directory bug fixed

---
+++ test/discreteBehaviorSimulatorTest/DiscreteActionSimulatorTest.java
@@ -0,0 +1,14 @@
+package discreteBehaviorSimulatorTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DiscreteActionSimulatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
